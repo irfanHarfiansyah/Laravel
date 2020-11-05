08b4490af6adc1b0b6131c35f8c2b80d16fbef80
irfanHarfiansyah <irfanharfiansyah37@gmail.com>
2020-11-05 22:58:52 +0700

    jobsheet7

--- cms/resources/views/layouts/Master.blade.php
+++ cms/resources/views/layouts/Master.blade.php
@@ -59,7 +59,8 @@
            </li>
           </li>
           <li class="nav-item dropdown">
-            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+             aria-haspopup="true" aria-expanded="false" v-pre>
                 {{ Auth::user()->name }} <span class="caret"></span>
             </a>
 
